a0de545501b1349251835a5f0dc1570a337ffd62
Arturs <archeelux@gmail.com>
2019-07-31 08:13:22 +0100

    153|154 COMPLETED

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "../public");
 
 app.use("/", express.static(publicDirPath));
 
-// app.get("/", (req, res) => {
-//     res.sendFile(publicDirPath);
-// });
+io.on("connection", () => {
+    console.log("new websocket connection");
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
 });
